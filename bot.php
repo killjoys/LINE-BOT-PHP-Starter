03619224bc00f680daf91ca754d5efd19e14177b
Chanon Deeprasertkul <downstait_miz@hotmail.com>
2016-11-01 16:05:48 +0700

    rollback msg

--- bot.php
+++ bot.php
@@ -17,16 +17,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages:[
-		        {
-		            "type":"text",
-		            "text":"Hello, user"
-		        },
-		        {
-		            "type":"text",
-		            "text":"May I help you?"
-		        }
-		    ]
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
